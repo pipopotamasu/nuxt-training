2313ecd04e756ccb2e4acaa6273cd9fcfc8c9eab
pipopotamasu <ec0156hx@gmail.com>
2018-07-08 17:56:44 +0900

    メールターゲットユーザ雛形

---
+++ components/mails/TargetUser.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="target-user">
+    <span>To</span>
+    <span class="user-icon">村上大和のアイコン</span>
+    <span class="user-fullname">村上大和</span>
+    <span class="user-sex-icon">男のアイコン</span>
+  </div>
+</template>
+
+<style>
+.target-user {
+  width: 100%;
+  text-align: left;
+  padding: 0.5em 1em;
+  background-color: blue;
+}
+
+span {
+  padding: 0 0.25em;
+}
+</style>
